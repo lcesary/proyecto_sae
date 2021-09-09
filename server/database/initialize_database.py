00fca948f91d2bcda912f527650cec0b77a92154
Enrique Cesary <ecesary@qsmbo.com>
2021-09-09 16:35:01 -0400

    fix: se cambio la cadena de conexion

--- server/database/initialize_database.py
+++ server/database/initialize_database.py
@@ -5,20 +5,12 @@ from configparser import ConfigParser
 from sqlalchemy.engine import create_engine
 
 from server.database import connection
-from server.persona.scripts import insertions as mpersonal_insertions
 from .models import Base
 from ..usuarios.scripts import insertions as user_insertions
-from ..chat.scripts import insertions as chat_insertions
-from ..ubicacion.scripts import insertions as ubicacion_insertions
-from ..pago.scripts import insertions as pago_insertions
 
 def main():
     reload_db()
     user_insertions()
-    # mpersonal_insertions()
-    chat_insertions()
-    ubicacion_insertions()
-    pago_insertions()
     print('Database created/updated correctly!')
 
 
