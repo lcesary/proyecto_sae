8579e788c64c98ac0739a4d102fd891ea00cfa23
HernandoPinaHenao <hpina@qsmbo.com>
2021-09-23 16:30:57 -0400

    feat: añadido data tables

--- server/operaciones/controllers.py
+++ server/operaciones/controllers.py
@@ -76,7 +76,12 @@ class HistorialController(CrudController):
         id = self.get_argument('id')
         #self.verif_privileges()
         result = self.manager(self.db).list_all()
-        result['usuario']=  UsuarioManager(self.db).listar_usuario(id)
+        result['usuario_perfil']=  UsuarioManager(self.db).sacar_estado(id)
+        #print(str(result['usuario_perfil'].fkrol))
+        rol = UsuarioManager(self.db).name_role(result['usuario_perfil'].fkrol)
+        result['usuario_perfil'].fkrol=rol
+        sucursal =SucursalManager(self.db).listar_id(int(result['usuario_perfil'].fksucursal))
+        result['usuario_perfil'].fksucursal =sucursal
         result['privileges'] = UsuarioManager(self.db).get_privileges(self.get_user_id(), self.request.uri)
         result['ubicaciones'] = BitacoraManager(self.db).obtenerUbicacionesUsuario(id)
         result.update(self.get_extra_data())
